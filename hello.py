a7dfddfacf9efe4298146eff504f1edd6f8a50b5
Nico <nico@nicox.org>
2020-04-23 23:58:22 +0200

    added wordpress, fixed waf recognition

--- hello.py
+++ hello.py
@@ -15,7 +15,7 @@ file1.close()
 #print (sites)
 interesting_headers = ['Server','Location','X-Powered-By','Strict-Transport-Security','X-XSS-Protection','X-Content-Type-Options','X-Frame-Options','Content-Security-Policy','X-Content-Security-Policy','X-WebKit-CSP','Referrer-Policy','Feature-Policy','Expect-CT']
 alphabet = list(map(chr, range(65, 90)))
-software = { "Typo3" : "typo3", "nextcloud" : "nextcloud"}
+software = { "Typo3" : "typo3", "Nextcloud" : "nextcloud", "Citrix Gateway" : "\/vpn\/login.js", "Wordpress" : "wp-content", "Nextcloud": "\/core\/js\/oc.js"}
 waf = {"F5" : "Support ID"}
 row = dict()
 column = dict()
@@ -26,11 +26,21 @@ def get_header(site,interesting_headers,workbook):
     site,None,
   )
   headers_response = response.headers
+  #print (headers_response)
   for x in interesting_headers:
     if x in headers_response:
       headervalue = headers_response[x]
+      #print (site + headervalue)
       cell = str(alphabet[column[x]]) + str(row[site])
       worksheet.write(cell, headervalue)
+  response_location = requests.get(
+    site,allow_redirects = False,)
+  headers_response_location = response_location.headers
+  if 'Location' in headers_response_location:
+    headervalue = headers_response_location['Location']
+    cell = str(alphabet[column['Location']]) + str(row[site])
+    worksheet.write(cell, headervalue)
+   # print (headervalue)
 
 ###### get the body informations
 def get_body(site,workbook,software):
@@ -38,6 +48,7 @@ def get_body(site,workbook,software):
     site,None,
   )
   body_response = str(response.content)
+  #print (body_response)
   for x in software:
     result = re.search(software[x],body_response, flags=re.IGNORECASE)
     if result is not None:
@@ -50,21 +61,21 @@ def req_waf(site,workbook):
   #/'%20or%201=1'
   url = site +"/'%20or%201=1'"
   cell = str(alphabet[column['waf']]) + str(row[site])
+ # print ('waftest jetzt')
   try:
     response = requests.get(
       url,None,
       )
   except requests.exceptions.RequestException as e:
     error = e
-    #print (error + ' when connecting to '+ site + ' for waf check')
+    print (error + ' when connecting to '+ site + ' for waf check')
     worksheet.write(cell, 'no response (IPS?)')
-  if error is None:
+  #print (response.content)
+  if error is '':
     waf_response = str(response.content)
     for x in waf:
       result = re.search(waf[x],waf_response, flags=re.IGNORECASE)
       if result is not None:
-#        print (result)
-
         worksheet.write(cell, x)
 
 
